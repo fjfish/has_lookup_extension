a60495d6cb3bebfc8fe44081025d2cbe40eb2378
Francis Fish <francis.fish@gmail.com>
2008-11-06 20:03:10 +0000

    add uninstall

--- uninstall.rb
+++ uninstall.rb
@@ -1,19 +1,21 @@
+require 'ftools'
 # Install hook code here
-# remove the migration
-RAILS_ROOT = "."
-puts "Destroying the migration"
-system "ruby script/destroy rspec_model lookup_codes"
+# create the migration
+puts "Removing the migration"
+lookup_name = Dir.glob("db/migrate/*lookup_code*.rb").first
+lookup_name = lookup_name.split(/\//).last
+lookup_name = lookup_name.split(/_/).first
+system "rake db:migrate:down VERSION=#{lookup_name}" rescue puts "Could not remove table!!"
+system "ruby script/destroy model lookup_code"
+# Find the lookup codes file and replace it with this
 # Create the loader
 # Create helpers/admin directory
-require 'ftools'
 puts "removing the loader"
 target_loader = "#{RAILS_ROOT}/app/helpers/admin/lookup_codes_loader.rb"
 if File.file?(target_loader)
   File.delete(target_loader)
 else
-  puts "loader does not exist?"
+  puts "loader did not exist?"
 end
-puts "removing app/helpers/admin directory if it isn't already there"
-Dir.rmdir("#{RAILS_ROOT}/app/helpers/admin") if File.directory?("#{RAILS_ROOT}/app/helpers/admin") rescue puts "Caught error $!"
 # Create the model
-puts "lookup codes uninstalled"
+puts "Lookup codes removed"
